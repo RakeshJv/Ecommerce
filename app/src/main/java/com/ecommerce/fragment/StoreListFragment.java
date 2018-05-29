8dcdaf94c196255d33c7b1331740dc28b1468723
Rvaishya <rakesh.vaishya@gmail.com>
2018-05-29 17:44:51 +0530

    Push Complete project

--- app/src/main/java/com/ecommerce/fragment/StoreListFragment.java
+++ app/src/main/java/com/ecommerce/fragment/StoreListFragment.java
@@ -13,12 +13,12 @@ import com.ecommerce.R;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link ListFragment.OnFragmentInteractionListener} interface
+ * {@link StoreListFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link ListFragment#newInstance} factory method to
+ * Use the {@link StoreListFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ListFragment extends Fragment {
+public class StoreListFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -30,7 +30,7 @@ public class ListFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public ListFragment() {
+    public StoreListFragment() {
         // Required empty public constructor
     }
 
@@ -40,11 +40,11 @@ public class ListFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ListFragment.
+     * @return A new instance of fragment StoreListFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ListFragment newInstance(String param1, String param2) {
-        ListFragment fragment = new ListFragment();
+    public static StoreListFragment newInstance(String param1, String param2) {
+        StoreListFragment fragment = new StoreListFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -60,17 +60,17 @@ public class ListFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        return inflater.inflate(R.layout.fragment_store_list, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
-        if (mListener != null)
-        {
+        if (mListener != null) {
             mListener.onFragmentInteraction(uri);
         }
     }
